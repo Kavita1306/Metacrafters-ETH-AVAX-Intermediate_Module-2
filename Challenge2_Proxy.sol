70f4f84d71fa14f7288b8926d851c885604ec91c
Kavita1306 <121939699+Kavita1306@users.noreply.github.com>
2023-07-13 15:21:28 +0530

    Update Challenge2_Proxy.sol

--- Challenge2_Proxy.sol
+++ Challenge2_Proxy.sol
@@ -1,1 +1,27 @@
- 
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+contract Proxy {
+
+  function upgradeTo(address newImplementation) public {
+  }
+
+}
+
+contract Upgradeable {
+
+  // The proxy contract that will be used to upgrade this contract
+  address public proxy;
+
+  constructor() {
+    proxy = address(new Proxy());
+  }
+
+  // Function to upgrade this contract
+  function upgrade(address newImplementation) public {
+    // Cast the proxy variable to the Proxy type
+    Proxy proxyContract = Proxy(proxy);
+    proxyContract.upgradeTo(newImplementation);
+  }
+
+} 
